1faac095fbb46258df75f9390676f2f2c67bbe72
Manh Thanh Nguyen <89306366+mthnhhh@users.noreply.github.com>
2021-08-22 20:29:46 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -104,7 +104,7 @@ $('#yes').click(function() {
         width: 900,
         padding: '3em',
         html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Tai sao nhi'>",
-        background: '#fff url("img/iput-IMG_0510.jpeg")',
+        background: '#fff url("img/iput-bg.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
               url("img/giphy2.gif")
