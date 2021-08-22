a30620b038dff79e25c52819191ea9886847472f
Manh Thanh Nguyen <89306366+mthnhhh@users.noreply.github.com>
2021-08-22 17:56:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Trân trọng kính gửi Hà Anh',
+    introDesc: `Đây là 1 đống linh tinh nhảm
+     nhí mà anh đã ăn cắp xong
+      tự biến nó thành của mình.`,
+    btnIntro: 'Vângg',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
