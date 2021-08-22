5d92b705ac06426e390fe4278f1530096634dfce
Manh Thanh Nguyen <89306366+mthnhhh@users.noreply.github.com>
2021-08-22 18:10:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ const CONFIG = {
     introTitle: 'Trân trọng kính gửi Hà Anh',
     introDesc: `Đây là 1 đống linh tinh nhảm
      nhí mà anh đã ăn cắp xong
-      tự biến nó thành của mình. Thì tự nhiên chả biết anh bị làm sao ý xong ngồi làm cái này cho em.`,
+     tự biến nó thành của mình. Thì tự nhiên chả biết anh bị làm sao ý xong ngồi làm cái này cho em.`,
     btnIntro: 'Vângg',
     title: 'Một câu hỏi mà thỉnh thoảng anh hay thắc mắc với bản thân',
     desc: 'Em có ghét anh khum :((',
     btnYes: 'Không, em iuu anh lắm',
     btnNo: 'Em ghét anhh',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Tại sao nhỉ hmmm',
     btnReply: 'Ấn để gửi',
-    reply: 'Anh có cái bụng rất nhiều mỡ ýyyy',
-    mess: 'Thế á =))) anh iu em nhiều lắm lunn',
+    reply: 'Anh đáng iu và có cái bụng rất rất nhiều mỡ luôn ýyyyyyyyy',
+    mess: 'Thế á =))) anh cũng iu em nhiều lắm lunn <3',
     messDesc: 'Hãy nhìn qua 1 chút về những stories nhớ',
     btnAccept: 'Ote',
     messLink: 'https://www.instagram.com/duskndawn._/' //link mess của các bạn. VD: https://m.me/nam.nodemy
